afbc78def42f5aa1acf6fe08f2ab79f0976b0d21
雍威 <2926385239@qq.com>
2022-05-12 10:58:47 +0800

    【todolist】给models增加docstring

--- todolist/models.py
+++ todolist/models.py
@@ -4,6 +4,15 @@ import django.utils.timezone as timezone
 
 
 class Item(models.Model):
+    """Item 事项
+
+    content: item's content. 事项内容
+    completed: return true if item is completed, or false. 是否已完成，若完成则返回true，否则返回false
+    deadline: item's deadline date, default today. 截止时间
+    create_date: create item date(include time) 创建时间
+    update_date: update item date(include time) 更新时间
+    tags: item's tags
+    """
     id = models.AutoField('id', primary_key=True)
     content = models.CharField('content', default='', max_length=255, null=True, help_text='事项')
     completed = models.BooleanField('is completed', default=False, null=True, help_text='是否已完成')
@@ -18,6 +27,10 @@ class Item(models.Model):
 
 
 class Tag(models.Model):
+    """Tag 标签
+
+    name: tag's name 标签名称
+    """
     id = models.AutoField('id', primary_key=True)
     name = models.CharField("tag name", default='', max_length=255, null=True, help_text='标签名称')
 

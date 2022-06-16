add126c0bf8f200843602ff42b0d2a2fe5915c27
雍威 <2926385239@qq.com>
2022-06-16 08:44:54 +0800

    [todolist] 使用基于mixins的类视图实现List（查找全部、创建1条记录）和Detail（获取、更新、删除1个记录）接口

--- todolist/views.py
+++ todolist/views.py
@@ -3,65 +3,44 @@ import logging
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import mixins, generics
 
 from todolist.serializers import ItemSerializer
 from todolist.models import Item
 
 
-class ItemListView(APIView):
+class ItemListView(mixins.ListModelMixin,
+                   mixins.CreateModelMixin,
+                   generics.GenericAPIView):
     """
     获取所有Item
     创建1个新Item
     """
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
 
-    # authentication_classes = [authentication.TokenAuthentication]
-    # permissions_classes = [permissions.IsAuthenticated]
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-    def get(self, request):
-        items = Item.objects.all()
-        # 序列化（从库中获取数据），items表示要序列化的对象，many=True表示items是多个对象的实例
-        # 如果items只有1个对象实例，则不需要many=True
-        serializer = ItemSerializer(items, many=True)
-        return Response({'status': True, 'message': '成功', 'data': serializer.data})
-
-    def post(self, request):
-        print(request.data.get('content1', 'x'))
-        # 反序列化（创建，将数据写入库中），加data参数，表示将data的值写入库中
-        serializer = ItemSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            logging.info(f"data: {serializer.data}")
-            return Response(serializer.data)
-        return Response({'status': False, 'message': '失败'})
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-class ItemDetailView(APIView):
+class ItemDetailView(mixins.RetrieveModelMixin,
+                     mixins.UpdateModelMixin,
+                     mixins.DestroyModelMixin,
+                     generics.GenericAPIView):
     """
     检索、更新或删除一个Item
     """
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
 
-    def get_object(self, pk):
-        try:
-            return Item.objects.get(pk=pk)
-        except Item.DoesNotExist:
-            raise Http404
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def get(self, request, pk):
-        item = self.get_object(pk)
-        serializer = ItemSerializer(item)
-        return Response({'status': True, 'message': '成功', 'data': serializer.data})
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-    def put(self, request, pk):
-        item = self.get_object(pk)
-        # 反序列化（更新），需要提供两个参数item
-        # 第一个参数item表示需要更新的实例，第二个参数data表示item的新值
-        serializer = ItemSerializer(item, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({'status': True, 'message': '成功', 'data': serializer.data})
-        return Response({'status': False, 'message': '失败'})
-
-    def delete(self, request, pk):
-        item = self.get_object(pk)
-        item.delete()
-        return Response({'status': True, 'message': '成功'})
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
